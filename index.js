428a9dd538a3cff90c44a513d48e1a2a29494cad
Jhana Harvey <jbharvey94@gmail.com>
2017-07-25 16:08:32 +0000

    Initial commit of website files

---
+++ index.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+
+    var navToggle = document.getElementById('nav-toggle');
+
+    var navItems = document.getElementById('nav-items')
+
+    //write the psuedo code
+
+    navToggle.addEventListener('click', function() {
+
+        navItems.classList.toggle('mobile-nav-hidden');
+    });
+    //the button will be clicked 
+
+    //when the button is clicked it will show the navabr
+}
